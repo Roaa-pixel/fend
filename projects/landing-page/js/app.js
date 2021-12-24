257ad3ed380ed44999c4daddec77d0fd90473d41
Roaa-pixel <72927350+Roaa-pixel@users.noreply.github.com>
2021-12-25 01:06:43 +0200

    Update app.js

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -54,6 +54,45 @@
 */
 
 // Build menu 
+$(document).ready(function(){     
+    var _dockPanel;
+    var content = document.createElement('div');
+    content.innerHTML = "";
+    var liststart = document.createElement('ul');
+    var listelement1 = document.createElement('li');
+    var listelement2 = document.createElement('li');
+    listelement1.innerHTML = '<a href="#tabs-1">One</a>';
+    listelement2.innerHTML = '<a href="#tabs-2">Two</a>';
+    liststart.appendChild(listelement1);
+    liststart.appendChild(listelement2);
+    var t1 = document.createElement('div');
+    var t2 = document.createElement('div');
+
+    t1.id = "tabs-1";
+    t2.id = "tabs-2";
+
+    t1.innerHTML = "Tab 1";
+    t2.innerHTML = "Tab 2";
+
+    content.appendChild(liststart);
+    content.appendChild(t1);
+    content.appendChild(t2);
+    var html = [
+      '<div>',
+      '<nav class="navbar navbar-default">',
+
+      '<ul class="nav navbar-nav">',
+      '<li class="active"><a href="#">Home</a></li>',
+      ' <li><a href="#">Page 1</a></li>',
+      '<li><a href="#">Page 2</a></li>',
+      '</ul>',
+
+      '</nav>',
+      '</div>'
+    ].join('\n');
+    $("#navbar1").append(html);
+    $("#navbar2").append(content);
+});
 
 // Scroll to section on link click
 
